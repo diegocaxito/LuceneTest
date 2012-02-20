42ba55af5c8dd6279112e0bd8c77112bf9300551
Diego Caxito <diegocaxito@gmail.com>
2012-02-20 11:06:16 -0200

    adicionando primeiros arquivos

---
+++ src/LuceneStudyTests/LuceneStudyTests/LockTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Lucene.Net.Analysis;
+using Lucene.Net.Index;
+using Lucene.Net.Store;
+using NUnit.Framework;
+using Lucene.Net;
+using Directory = Lucene.Net.Store.Directory;
+
+namespace LuceneStudyTests
+{
+    [TestFixture]
+    public class LockTest
+    {
+        private Directory diretorio;
+
+        [SetUp]
+        public void IniciarTeste()
+        {
+            diretorio = FSDirectory.Open(new DirectoryInfo("~/"));
+        }
+
+        [Test, ExpectedException(typeof(LockObtainFailedException))]
+        public void TestWriteLock_QuandoTentarCriarMaisDeUmEscritorDeIndiceParaOMesmoDiretorio_NaoDevePermitir_e_DeveGerarExcecao()
+        {
+            using (var escritorIndice = new IndexWriter(diretorio, new SimpleAnalyzer(), IndexWriter.MaxFieldLength.UNLIMITED))
+            {
+                IndexWriter outroEscritorIndice = null;
+
+                outroEscritorIndice = new IndexWriter(diretorio, new SimpleAnalyzer(),
+                                                      IndexWriter.MaxFieldLength.UNLIMITED);
+            }
+        }
+    }
+}
