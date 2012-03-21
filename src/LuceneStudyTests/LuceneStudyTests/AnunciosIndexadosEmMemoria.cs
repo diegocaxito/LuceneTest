fb2d50795ee9ad2b01c2cecee73355012686a282
Diego Caxito <diegocaxito@gmail.com>
2012-03-21 00:46:17 -0300

    testes com diferentes tipos de pesquisa

---
+++ src/LuceneStudyTests/LuceneStudyTests/AnunciosIndexadosEmMemoria.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Lucene.Net.Analysis;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+
+namespace LuceneStudyTests
+{
+    public class AnunciosEmMemoria : IDisposable
+    {
+        public const string Id = "Id";
+        public const string TipoImovel = "tipoImovel";
+        public const string Bairro = "bairro";
+        public const string Cidade = "cidade";
+        public const string Estado = "estado";
+        public const string Preco = "preco";
+
+        public Directory Diretorio { get; set; }
+
+        public AnunciosEmMemoria()
+        {
+            Diretorio = new RAMDirectory();
+            using (var indexWriter = new IndexWriter(Diretorio, new SimpleAnalyzer(), IndexWriter.MaxFieldLength.UNLIMITED))
+                CriarBaseDocumentosImoveisEmMemoria(indexWriter);
+        }
+
+        public Document ObterDocumento(int id, string tipoImovel, string bairro = "Bela Vista", string cidade = "São Paulo", string estado = "SP", double preco = 200)
+        {
+            var documento = new Document();
+            documento.Add(new Field(Id, id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            documento.Add(new Field(TipoImovel, tipoImovel, Field.Store.YES, Field.Index.ANALYZED));
+            documento.Add(new Field(Bairro, bairro, Field.Store.YES, Field.Index.ANALYZED));
+            documento.Add(new Field(Cidade, cidade, Field.Store.YES, Field.Index.ANALYZED));
+            documento.Add(new Field(Estado, estado, Field.Store.YES, Field.Index.ANALYZED));
+
+            var campoPreco = new NumericField(Preco);
+            campoPreco.SetDoubleValue(preco);
+            documento.Add(campoPreco);
+            
+            return documento;
+        }
+
+        public void CriarBaseDocumentosImoveisEmMemoria(IndexWriter indexWriter)
+        {
+            var documento = ObterDocumento(1, "Residencial", preco:2400);
+            var documento2 = ObterDocumento(2, "Comercial", preco: 200000);
+            var documento3 = ObterDocumento(3, "Empreendimento");
+            var documento4 = ObterDocumento(4, "Residencial/Comercial");
+            var documento5 = ObterDocumento(5, "Residencial", "Funcionarios", "Montes Claros", "MG");
+            var documento6 = ObterDocumento(6, "Comercial", "Funcionarios", "Montes Claros", "MG");
+            var documento7 = ObterDocumento(7, "Residencial", "Melo", "Montes Claros", "MG");
+            var documento8 = ObterDocumento(8, "Residencial", "Funcionarios", "Belo Horizonte", "MG");
+            var documento9 = ObterDocumento(9, "Comercial", "Centro", "Campinas", preco: 150);
+            var documento10 = ObterDocumento(10, "Comercial", "Pampulha", "Belo Horizonte", "MG");
+            indexWriter.AddDocument(documento);
+            indexWriter.AddDocument(documento2);
+            indexWriter.AddDocument(documento3);
+            indexWriter.AddDocument(documento4);
+            indexWriter.AddDocument(documento5);
+            indexWriter.AddDocument(documento6);
+            indexWriter.AddDocument(documento7);
+            indexWriter.AddDocument(documento8);
+            indexWriter.AddDocument(documento9);
+            indexWriter.AddDocument(documento10);
+        }
+
+        public void ExtrairExplicacaoQueryComTermoPorCidade(Directory diretorio, Query query)
+        {
+            using (var indexSearcher = new IndexSearcher(diretorio, true))
+            {
+                var topDocs = indexSearcher.Search(query, 10);
+
+                foreach (var scoreDoc in topDocs.ScoreDocs)
+                {
+                    var explanation = indexSearcher.Explain(query, scoreDoc.doc);
+                    var document = indexSearcher.Doc(scoreDoc.doc);
+                    Console.WriteLine("------\n{0}\n{1}", document.Get(TipoImovel), explanation);
+                }
+            }
+        }
+
+        public static void ApresentarDiretorio()
+        {
+            var apresentacao = new AnunciosEmMemoria();
+            string opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Projeto de testes de tipo de query\nExpecifique a consulta\nConsulta: ");
+                string consulta = Console.ReadLine();
+                Console.WriteLine("\n\nEscolha o tipo de exibição\n1 - Explicação de query em cima do nome da cidade\n2 - Resultado de full text search textual");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "2":
+                        break;
+                    default:
+                        apresentacao.ExbibicaoDeExplicacao(consulta);
+                        break;
+                }
+                Console.ReadKey();
+
+                Console.WriteLine("\n\nPressione 1 para sair ou qualquer outra tecla para continuar...");
+                opcao = Console.ReadLine();
+            } while (opcao != "1");
+        }
+
+        public void ExbibicaoDeExplicacao(string consulta)
+        {
+            var parser = new QueryParser(LuceneUtil.ObterVersao(), Cidade, new SimpleAnalyzer());
+            var query = parser.Parse(consulta);
+            Console.WriteLine("Query: {0}", query);
+            ExtrairExplicacaoQueryComTermoPorCidade(Diretorio, query);
+        }
+
+        public void Dispose()
+        {
+            if (Diretorio != null) Diretorio.Close();
+        }
+    }
+}
---
+++ src/LuceneStudyTests/LuceneStudyTests/TermQueryTeste.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using NUnit.Framework;
+using Lucene.Net;
+
+namespace LuceneStudyTests
+{
+    [TestFixture]
+    public class TermQueryTeste
+    {
+        private AnunciosEmMemoria anunciosEmMemoria;
+
+        [TestFixtureSetUp]
+        public void IniciarTeste()
+        {
+            anunciosEmMemoria = new AnunciosEmMemoria();
+        }
+
+        [TestFixtureTearDown]
+        public void FinalizarTeste()
+        {
+            anunciosEmMemoria.Dispose();
+        }
+
+        [Test]
+        public void Keyword_QuandoUtilizarBuscaPorTerm_e_TermoForUtilizadoComoId_DeveRetornarDados()
+        {
+            /* 
+             * Pesquisa por term queries são recomendados para buscar Ids de itens
+             */
+            Directory diretorio = anunciosEmMemoria.Diretorio;
+            using (var searcher = new IndexSearcher(diretorio, true))
+            {
+                var termo = new Term(AnunciosEmMemoria.Id, "5");
+                var query = new TermQuery(termo);
+                var topDocs = searcher.Search(query, 10);
+                Assert.AreEqual(1, topDocs.TotalHits, "Espera ter encontrado por Montes Claros");
+            }
+        }
+
+        [Test]
+        public void TermRangeQuery_QuandoPassarRangeDe_b_ate_e_DeveRetornarSeisItens()
+        {
+            var diretorio = anunciosEmMemoria.Diretorio;
+            using (var searcher = new IndexSearcher(diretorio, true))
+            {
+                var termRangeQuery = new TermRangeQuery(AnunciosEmMemoria.Cidade, "a", "e", true, true);
+                var docsThatMatch = searcher.Search(termRangeQuery, 100);
+
+                var textoComErro = new StringBuilder("Anúncios encontrados\n");
+
+                foreach (var topDoc in docsThatMatch.ScoreDocs)
+                {
+                    var documentoEncontrado = searcher.Doc(topDoc.doc);
+                    textoComErro.AppendLine(string.Format("TipoImovel: {0}\tCidade: {1}", documentoEncontrado.Get(AnunciosEmMemoria.TipoImovel), documentoEncontrado.Get(AnunciosEmMemoria.Cidade)));
+                }
+
+                Assert.AreEqual(6, docsThatMatch.TotalHits, textoComErro.ToString());
+            }
+        }
+
+        [Test]
+        public void NumericRangeQuery_QuandoAnalistarEntre_2000_e_10000_e_VerificarTesteInclusivo_DeveRetornarApenasUmitem()
+        {
+            var diretorio = anunciosEmMemoria.Diretorio;
+            using(var searcher = new IndexSearcher(diretorio, true))
+            {
+                var rangeNumeros = NumericRangeQuery.NewDoubleRange(AnunciosEmMemoria.Preco, 2000, 10000, true, true);
+                var matches = searcher.Search(rangeNumeros, 10);
+                Assert.AreEqual(1, matches.TotalHits);
+            }
+        }
+
+        public void BooleanQueries_QuandoCombinarSituacoes_DeveRetornarResultado(){}
+    }
+}
