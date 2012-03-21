a44dbe2a9d51cf5efd91a4c79b5bfce72be5abe4
Diego Caxito <diegocaxito@gmail.com>
2012-03-21 14:43:28 -0300

    teste de phraseQuery

--- src/LuceneStudyTests/LuceneStudyTests/TermQueryTeste.cs
+++ src/LuceneStudyTests/LuceneStudyTests/TermQueryTeste.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Lucene.Net.Analysis;
+using Lucene.Net.Documents;
 using Lucene.Net.Index;
 using Lucene.Net.Search;
 using Lucene.Net.Store;
@@ -68,7 +70,7 @@ namespace LuceneStudyTests
         public void NumericRangeQuery_QuandoAnalistarEntre_2000_e_10000_e_VerificarTesteInclusivo_DeveRetornarApenasUmitem()
         {
             var diretorio = anunciosEmMemoria.Diretorio;
-            using(var searcher = new IndexSearcher(diretorio, true))
+            using (var searcher = new IndexSearcher(diretorio, true))
             {
                 var rangeNumeros = NumericRangeQuery.NewDoubleRange(AnunciosEmMemoria.Preco, 2000, 10000, true, true);
                 var matches = searcher.Search(rangeNumeros, 10);
@@ -84,7 +86,7 @@ namespace LuceneStudyTests
              * Acontece uma combinação de duas queries em uma.
              * Para alterar o comportamento do teste você pode apenas alterar entre BooleanClause.Occur.MUST ou BooleanClause.Occur.SHOULD ou BooleanClause.Occur.MUST_NOT
              */
-            
+
             var pesquisarAnuncios = new TermQuery(new Term(AnunciosEmMemoria.Cidade, "montes"));
             var pesquisa = NumericRangeQuery.NewDoubleRange(AnunciosEmMemoria.Preco, 1000, 100000, true, true);
             var booleanQuery = new BooleanQuery();
@@ -113,8 +115,62 @@ namespace LuceneStudyTests
             return false;
         }
 
-        //[Test]
-        //publix 
+        [TestCase(new[] { "quick", "lazy" }, "the quick brown fox jumped over the lazy dog", 7,TestName = "Quick and lazy with distance of 7")]
+        [TestCase(new[] { "Sócrates", "amigo" }, "Sócrates é meu amigo, mas eu sou amigo da verdade", 10, TestName = "Sócrates e amigo com distancia de 10")]
+        [TestCase(new[] { "Apartamento", "quartos" }, "Apartamento com quatro quartos", 2, TestName = "Apartamento e quarto com distância de 2")]
+        public void PhraseQueryText(string[] frase, string textoParaProcurar, int distanciaEntrePalavras)
+        {
+            const string texto = "texto";
+            using (var directory = new RAMDirectory())
+            {
+                using (var indexWriter = new IndexWriter(directory, new WhitespaceAnalyzer(), IndexWriter.MaxFieldLength.UNLIMITED))
+                {
+                    var doc = new Document();
+                    doc.Add(new Field(texto, textoParaProcurar, Field.Store.YES, Field.Index.ANALYZED));
+                    indexWriter.AddDocument(doc);
+                }
 
+                using (var searcher = new IndexSearcher(directory, true))
+                {
+                    var phraseQuery = new PhraseQuery();
+                    phraseQuery.SetSlop(distanciaEntrePalavras);
+                    
+                    foreach (var palavra in frase)
+                        phraseQuery.Add(new Term(texto, palavra));
+
+                    var matches = searcher.Search(phraseQuery, 10);
+                    var encontrou = matches.TotalHits > 0;
+                    var textoResultado = NaoEncontrou(textoParaProcurar, distanciaEntrePalavras, frase);
+                    Assert.IsTrue(encontrou, textoResultado);
+                }
+            }
+        }
+
+        private static string NaoEncontrou(string textoParaProcurar, int distanciaEntrePalavras, string[] frase)
+        {
+            var textoResultado = new StringBuilder();
+            textoResultado
+                .AppendLine(
+                    string.Format("\nNão encontrou \nDistância entre palavras: \"{0}\" \nTexto: \"{1}\"", 
+                                  distanciaEntrePalavras,
+                                  textoParaProcurar));
+
+            AdicionarPalavras(frase, textoResultado);
+
+            return textoResultado.ToString();
+        }
+
+        private static void AdicionarPalavras(IEnumerable<string> frase, StringBuilder textoResultado)
+        {
+            textoResultado.AppendLine("As palavras: ");
+            foreach (var palavra in frase)
+                textoResultado.AppendLine(string.Format("\t - {0}", palavra));
+        }
+
+        [Test]
+        public void WildcardQueryTest()
+        {
+
+        }
     }
 }
