dccfeb409f7d29c90461cc8fd8993c2d0446cba5
Diego Caxito <diegocaxito@gmail.com>
2012-03-23 13:46:29 -0300

    adicionando arquivo de bibliotecas

---
+++ src/LuceneStudyTests/AnlyzerDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnlyzerDemo
+{
+    public class AnalyzerDemo
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        private static String[] exemples = {
+                                               "The quick brown fox jumped over the lazy dog",
+                                               "XY&Z Corporation - xyz@example.com"
+                                           };
+
+        //private static Ana
+    }
+}
